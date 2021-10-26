752e82704897df196ce0354265d56418550f966d
Mati <matias.abarca2018@gmail.com>
2021-10-26 14:04:05 -0300

    Firestore configurado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 import { ComponentsModule } from './components/components.module';
 // Importar register de amgular common, el localEs es un nombre dado por mi.
 import localEs from "@angular/common/locales/es-CL";
 import { registerLocaleData } from "@angular/common";
+import { environment } from 'src/environments/environment';
 
 registerLocaleData( localEs );//Funcion para implementes el es-CL
 
@@ -18,7 +20,9 @@ registerLocaleData( localEs );//Funcion para implementes el es-CL
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'es-CL' }],
   bootstrap: [AppComponent]
